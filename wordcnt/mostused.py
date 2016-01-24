f21a4ee3d28bff23baaa94acf54764d37a488d9b
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-24 17:44:45 -0500

    all the argument handling is now done with argparse

--- wordcnt/mostused.py
+++ wordcnt/mostused.py
@@ -1,12 +1,8 @@
 # take a file and figure out the four most used words.
 import sys
-# import nltk
 import re
 import string
-from collections import OrderedDict
-
-## import argparse ## TODO use this to make argument parsing cleaner once
-                   ## you get the NLTK stuff worked out
+import argparse
 
 ## this function takes a word and gives a canonical syntax for it--so
 ## removes punctionation, normalizes case, etc.
@@ -31,15 +27,14 @@ def incr (word, d):
 def sortpl (l):
     return sorted(l, reverse=True, key=lambda x : x[1])
 
-def biggest (k , items):
-    ## abort if the query makes no sense
-    if k > len(items):
-        raise Exception('trying to compute the ' + str(k) +
-                        'most used words, but there are only ' + str(len(d)) +
-                        ' unique words in the corpus')
 
-    ## otherwise, grab the first k entries, then traverse the rest of the
-    ## items updating as we go to find the biggest k
+# assume that k makes sense relative to items.
+## computes the k most-occuring
+# pairs in a list of pairs, where the second component is a number. assumes
+# that 0 <= k <= |items|.
+def biggest (k , items):
+    ## grab the first k entries, then traverse the rest of the items
+    ## updating as we go to find the biggest k
     so_far = sortpl(items[:k])
 
     for elem in items[k:]:
@@ -48,51 +43,80 @@ def biggest (k , items):
 
     return so_far
 
-## TODO: this is the number of words i want to find; take it as an
-## argument, but it's up here so i don't hard-code it later.
-k = 4
+## check while parsing the arguments that the number given is a nat. zero
+## is ok, oddly enough!
+def nat (string) :
+    value = int(string)
+    if value < 0:
+        msg = "%r is not a natural number" % string
+        raise argparse.ArgumentTypeError(msg)
+    return value
 
-if len (sys.argv) == 1 :
-    ## currently assuming that we get exactly one arg, which is a path to
-    ## the corpus file
-    print 'please provide one argument, a path to the file that you want to traverse'
-else:
-    # open the file from the command line arguments or bail if we can't
-    try:
-        f = open(sys.argv[1], 'r')
-    except IOError:
-        print 'Please supply a path to a real file'
+#####################
 
-    # traverse the whole file, adding canonical forms of valid words into a
-    # dictionary counting the number of appearances
-    d = dict()
-    for line in f:
-        ## get rid of ASCII em and en dashes before breaking into words, so
-        ## each half is its own word
-        line = (line.replace("---", " ")).replace("--", " ")
+## describe and parse arguments from the command line
+parser = argparse.ArgumentParser(description="Count the n most-used words in a corpus of English text")
+parser.add_argument("filename", help="path to the plain text file with the corpus to analyze")
+parser.add_argument("-n" , "--number",
+                    help="number of most frequently used words to compute. defaults to 4.",
+                    type=nat,
+                    default=4)
+parser.add_argument("-p" , "--pdf",
+                    help="treat the file as PDF rather than plain text. the results may be different depending on the quality of the PDF. default is false.",
+                    action="store_true",
+                    default=False)
+args = parser.parse_args()
 
-        for word in line.split():
-            clean_word = clean(word)
-            if clean_word == None:
-                #ignore words that don't parse
-                continue
-            else:
-                # add or update words that do parse
-                incr(clean_word,d)
+# open the file from the command line arguments or bail if we can't
+try:
+    f = open(args.filename, 'r')
+except IOError:
+    print 'Please supply a path to a real file'
 
-    # all the interesting data is in the dict now, so free this up
-    f.close()
+if args.pdf == True:
+    print "aw crap"
+    exit(1)
 
-    ## TODO: finish using this to debug the string parsing stuff
-    # for k, v in d.items():
-    #     print k, '|->', v
-    # print 'unique words: ', len(d)
+# traverse the whole file, adding canonical forms of valid words into a
+# dictionary counting the number of appearances
+d = dict()
+for line in f:
+    ## get rid of ASCII em and en dashes before breaking into words, so
+    ## each half is its own word
+    line = (line.replace("---", " ")).replace("--", " ")
 
-    # travese the dictionary to find the k most frequently used words, where
-    # k is taken on the command line but defaults to four.
+    for word in line.split():
+        clean_word = clean(word)
+        if clean_word == None:
+            #ignore words that don't parse
+            continue
+        else:
+            # add or update words that do parse
+            incr(clean_word,d)
 
-    print "the four most-used words are: "
-    for key , val in biggest(k,d.items()):
-        print ('\t"' + key + '", which was used '
-               + (str(val))
-               + ' time' + ("" if val == 1 else "s"))
+# all the interesting data is in the dict now, so free this up
+f.close()
+
+## TODO: finish using this to debug the string parsing stuff
+# for k, v in d.items():
+#     print k, '|->', v
+# print 'unique words: ', len(d)
+
+# travese the dictionary to find the k most frequently used words, where
+# k is taken on the command line but defaults to four.
+
+## abort if the query makes no sense. note that we can't check this stuff
+## until we build the dictionary, because whether or not the argument
+## args.number is sensible depends on the number of unique words.
+if args.number > len(d):
+    raise Exception('trying to compute the ' + str(args.number) +
+                    'most used words, but there are only ' + str(len(d)) +
+                    ' unique words in the corpus')
+
+print ("the " + str(args.number) + " most-used word" +
+       (" is" if args.number == 1 else "s are") +
+       ":")
+for key , val in biggest(args.number, d.items()):
+    print ('\t"' + key + '", which was used '
+           + (str(val))
+           + ' time' + ("" if val == 1 else "s"))
