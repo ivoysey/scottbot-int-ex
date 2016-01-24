36f83119afc0e8757bfdb2ffbc8ba9734be808ff
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-24 14:43:57 -0500

    The core of this works mostly, but I'm not happy with it for a couple of reasons:
      - there are still some strings that are being recognized as words that i
        would rather not be. i've been fussing with regexes to dike out
        puntuation and that sort of thing but i'm not happy with it yet.
    
        haven't decided if it's ok to do this sort of thing by hand or if i
        should use nltk. it's a big hammer and takes longer to load than the
        entire rest of the program takes to run.
    
        i played with it and it doesnt seem to add a ton; still leaves extenal
        punctuation like ',' and '.' and capitalization to fix, which i can do
        by my self just as well. but maybe i just haven't find the right API
        page yet.
    
      - if there are n unique words, for whatever definition, after you read in
        the file, this solution is O(n log n) in time because it sorts the
        dictionary. it also makes a full copy of the dictionary because it
        sorts it by value, and it seems that the standard way to do that in
        python is to dump everything into a OrderedDict in sorted order.
    
        there's a O(n) solution that i haven't implemented that uses
        QuickSelect, which is a little bit more delicate to do correctly. i
        haven't decided between that solution and a solution that traverses the
        dictionary and finds the k-max elements, rather than the full select. i
        think that might actually be harder to understand the code for, and
        have about the same runtime depending on what you have to do to update
        the maxes as you go.
    
      - need a nice way to handle arguments and possible PDF files

--- wordcnt/mostused.py
+++ wordcnt/mostused.py
@@ -1,11 +1,12 @@
 # take a file and figure out the four most used words.
 import sys
-## import nltk
+# import nltk
 import re
+import string
+from collections import OrderedDict
 
 ## import argparse ## TODO use this to make argument parsing cleaner once
                    ## you get the NLTK stuff worked out
-
 
 ## this function takes a word and gives a canonical syntax for it--so
 ## removes punctionation, normalizes case, etc.
@@ -13,7 +14,15 @@ import re
 ## TODO: looking at the contents of the keyset this generates this is
 ## probably harder than i think; i bet nltk has a function to do it.
 def clean (s):
-    return s.lower()
+    ## ignore strings containing at least one digit or URL artifacts
+    digits = re.compile('.*\d+.*|http')
+    if (digits.match(s)):
+        return None
+    else :
+        # otherwise, remove punctuation except hyphens  and normalize case
+        s = s.translate(None, string.punctuation.replace("-", ""))
+        s = s.lower()
+        return s
 
 if len (sys.argv) == 1 :
     ## currently assuming that we get exactly one arg, which is a path to
@@ -24,17 +33,34 @@ else:
         f = open(sys.argv[1], 'r')
         d = dict()
         for line in f:
-            words = line.split()
-            for word in words:
-                clean_word = clean(word)
-                if clean_word in d:
-                    d[clean_word] += 1
-                else:
-                    d[clean_word] = 1
-        f.close()
-        print d
+            ## get rid of ASCII em and en dashes
+            line = (line.replace("---", " ")).replace("--", " ")
 
-    except ValueError:
-        print 'Please supply integer arguments'
+            for word in line.split():
+                clean_word = clean(word)
+                if clean_word == None:
+                    #ignore words that don't parse
+                    continue
+                else:
+                    # add or update words that do parse
+                    if clean_word in d:
+                        d[clean_word] += 1
+                    else:
+                        d[clean_word] = 1
+
+        # all the interesting data is in the dict now, so free this up
+        f.close()
+
+        # for k, v in d.items():
+        #     print k, '|->', v
+        # print 'unique words: ', len(d)
+
+        # sort the dictionary by value in the standard way
+        od = OrderedDict(sorted(d.items(), reverse=True, key=lambda t: t[1]))
+        print "the four most used words are: "
+        for k , v in (od.items())[:4]:
+            print '\t"' + k + '", which was used ' + (str(v)) + ' times'
+
+
     except IOError:
         print 'Please supply a path to a real file'
