e591baf2df876bb87d056276d39e15a5af622967
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-25 10:43:43 -0500

    refactoring

--- wordcnt/mostused.py
+++ wordcnt/mostused.py
@@ -2,7 +2,7 @@ import re
 import argparse
 import itertools
 
-from util import clean , fexists
+from util import clean , fexists , baseargs
 
 # increment a value in a dictionary or set it to 1 if it's not there.
 def incr (word, d):
@@ -41,30 +41,12 @@ def nat (string) :
 ## describe and parse arguments from the command line
 parser = argparse.ArgumentParser(description="Count the n most-used words in" +
                                  "a corpus of English text.")
-parser.add_argument("filename",
-                    help="path to the file with the corpus to analyze. " +
-                         "by default, assumed to be plain text.",
-                    type=fexists)
+baseargs (parser)
 parser.add_argument("-n" , "--number",
                     help="number of most frequently used words to compute. "
                           + "defaults to 4.",
                     type=nat,
                     default=4)
-parser.add_argument("-p" , "--pdf",
-                    help="treat the file as PDF rather than plain text. the " +
-                         "results may be different depending on the quality " +
-                         "of the PDF. default is false. requires the package" +
-                         " pdfrw.",
-                    action="store_true",
-                    default=False)
-
-parser.add_argument("-g", "--gutenberg",
-                    help="indicates that the file came from Project Gutenberg"+
-                         ", in which case we ignore their header and footer."+
-                         " this will only work when combined with the PDF " +
-                         "option if we get a good parse. default is off.",
-                    action="store_true",
-                    default=False)
 args = parser.parse_args()
 
 # get the text of the corpus from either a plain text or PDF file
--- wordcnt/util.py
+++ wordcnt/util.py
@@ -33,6 +33,27 @@ def noun (string):
     #TODO: use nltk here
     return string
 
+# adds basic arguments to an argparser
+def baseargs (p):
+    p.add_argument("filename",
+                    help="path to the file with the corpus to analyze. " +
+                         "by default, assumed to be plain text.",
+                    type=fexists)
+    p.add_argument("-p" , "--pdf",
+                    help="treat the file as PDF rather than plain text. the " +
+                         "results may be different depending on the quality " +
+                         "of the PDF. default is false. requires the package" +
+                         " pdfrw.",
+                    action="store_true",
+                    default=False)
+    p.add_argument("-g", "--gutenberg",
+                    help="indicates that the file came from Project Gutenberg"+
+                         ", in which case we ignore their header and footer."+
+                         " this will only work when combined with the PDF " +
+                         "option if we get a good parse. default is off.",
+                    action="store_true",
+                    default=False)
+
 # this is https://gist.github.com/jmcarp/7105045. the standard docs for
 # how to use pdfminer give an example of how to use it, but it's slightly
 # busted. this is what the internet seems to have settled on for how to
